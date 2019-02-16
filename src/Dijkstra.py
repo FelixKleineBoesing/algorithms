c080d5ac86fe3bc1523913165cf4aa8841b0fa94
Felix Kleine Boesing <felix.boesing@t-online.de>
2019-02-16 16:08:44 +0100

    edit dijkstra

--- src/Dijkstra.py
+++ src/Dijkstra.py
@@ -20,20 +20,51 @@ def load_file(file: str):
 
 class Dijkstra:
 
-    def __init__(self, data: [[]]):
-        assert type(data) == list
-        self.data = data
-        self.graph = {}
-        self.vertex_source = None
+    def __init__(self, data: {}):
+        assert type(data) == dict
+        self.graph = data
+        self.vertex_source = next(iter(self.graph.keys()))
 
     def construct_graph(self):
         pass
 
-    def calculate_dijkstra_shortets_path(self):
-        pass
+    def calculate_dijkstra_shortest_path(self):
+
+        src = self.vertex_source
+        visited = []
+
+        paths_shortest = {vtx: (9999999999, []) for vtx in self.graph.keys()}
+        paths_shortest[src] = (0, [])
+        visited += [src]
+
+        while len(self.graph.keys()-visited) > 0:
+            src = -1
+            edge_minimum = ()
+            for vtx in visited:
+                for edge in self.graph[vtx]:
+                    if edge["node"] in visited:
+                        continue
+                    if not edge_minimum or paths_shortest[vtx][0] + edge["length"] < edge_minimum[1]:
+                        edge_minimum = (edge["node"], paths_shortest[vtx][0] + edge["length"])
+                        src = vtx
+            paths_shortest[edge_minimum[0]] = (edge_minimum[1], paths_shortest[src][1] + [edge_minimum[0]])
+            visited += [edge_minimum[0]]
+        return paths_shortest
+
 
 if __name__ == '__main__':
     data = load_file("data/dijkstraData.txt")
     dijkstra = Dijkstra(data)
     dijkstra.construct_graph()
-    shortest_paths = dijkstra.calculate_dijkstra_shortets_path()
+    shortest_paths = dijkstra.calculate_dijkstra_shortest_path()
+    actual = {vertex: distance[0] for (vertex, distance) in shortest_paths.items()}
+    print(actual[7])
+    print(actual[37])
+    print(actual[59])
+    print(actual[82])
+    print(actual[99])
+    print(actual[115])
+    print(actual[133])
+    print(actual[165])
+    print(actual[188])
+    print(actual[197])
