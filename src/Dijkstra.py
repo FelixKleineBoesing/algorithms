8058c1fccf579b33fe99367ab31135b619ab1d4c
Felix <felix.boesing@t-online.de>
2019-02-16 17:48:47 +0100

    update Dijkstra

--- src/Dijkstra.py
+++ src/Dijkstra.py
@@ -1,5 +1,3 @@
-from ast import literal_eval
-
 def load_file(file: str):
     '''
     load file for counting
@@ -24,47 +22,46 @@ class Dijkstra:
         assert type(data) == dict
         self.graph = data
         self.vertex_source = next(iter(self.graph.keys()))
-
-    def construct_graph(self):
-        pass
+        self.paths_shortest = None
 
     def calculate_dijkstra_shortest_path(self):
-
+        # get source node
         src = self.vertex_source
         visited = []
-
-        paths_shortest = {vtx: (9999999999, []) for vtx in self.graph.keys()}
+        # init shortest paths, source with distance 0 and every other vertex with 1000000
+        # tuple: (Distance, previous nodes)
+        paths_shortest = {vtx: (1000000, []) for vtx in self.graph.keys()}
         paths_shortest[src] = (0, [])
         visited += [src]
 
+        # while there are unvisited vertices
         while len(self.graph.keys()-visited) > 0:
             src = -1
             edge_minimum = ()
             for vtx in visited:
+                # check every nodes distance that is connected with the vtx-node
                 for edge in self.graph[vtx]:
                     if edge["node"] in visited:
+                        # continue of node already visited
                         continue
                     if not edge_minimum or paths_shortest[vtx][0] + edge["length"] < edge_minimum[1]:
                         edge_minimum = (edge["node"], paths_shortest[vtx][0] + edge["length"])
+                        # set new source for path
                         src = vtx
             paths_shortest[edge_minimum[0]] = (edge_minimum[1], paths_shortest[src][1] + [edge_minimum[0]])
             visited += [edge_minimum[0]]
+        self.paths_shortest = paths_shortest
         return paths_shortest
 
 
 if __name__ == '__main__':
-    data = load_file("data/dijkstraData.txt")
+    data = load_file("../data/dijkstraData.txt")
     dijkstra = Dijkstra(data)
     dijkstra.construct_graph()
     shortest_paths = dijkstra.calculate_dijkstra_shortest_path()
-    actual = {vertex: distance[0] for (vertex, distance) in shortest_paths.items()}
-    print(actual[7])
-    print(actual[37])
-    print(actual[59])
-    print(actual[82])
-    print(actual[99])
-    print(actual[115])
-    print(actual[133])
-    print(actual[165])
-    print(actual[188])
-    print(actual[197])
+    vertices = [7, 37, 59, 82, 99, 115, 133, 165, 188, 197]
+    distances = []
+    for vertex in vertices:
+        distances += [str(shortest_paths[vertex][0])]
+
+    print(",".join(distances))
