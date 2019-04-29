70a857a2b662b7f1beb4ae971405cb1e0e8fa639
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-04-29 19:17:24 +0200

    intermediate work

--- c/apsp.c
+++ c/apsp.c
@@ -10,4 +10,19 @@ double apsp(double m, double b, double *x, double *y, double *yerr, int N) {
     }
 
     return result;
+};
+
+double apsp(float arr, int number_vtx) {
+
+    float arr_out[arr.sizeof]
+
+    for (i = 0; i < number_vtx; i++) {
+        for (j = 0; j < number_vtx; j++) {
+            for (k = 0; k < number_vtx; k++) {
+                /* TODO do_magic()*/
+            }
+        }
+    }
+
+    return arr_out
 }
