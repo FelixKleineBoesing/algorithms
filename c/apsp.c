493f52da5dbad9d559b1595a1998206b2cf659a0
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-05-03 23:49:22 +0200

    intermediate work at apsp c function

--- c/apsp.c
+++ c/apsp.c
@@ -1,27 +1,16 @@
 #include "apsp.h"
 
-double apsp(double m, double b, double *x, double *y, double *yerr, int N) {
-    int n;
-    double result = 0.0, diff;
 
-    for (n = 0; n < N; n++) {
-        diff = (y[n] - (m * x[n] + b)) / yerr[n];
-        result += diff * diff;
-    }
-
-    return result;
-};
-
-double apsp(float *arr) {
-
-    for (i = 0; i < number_vtx; i++) {
-        for (j = 0; j < number_vtx; j++) {
-            for (k = 0; k < number_vtx; k++) {
-                if (arr[i, j] > (arr[i, k] + arr[k, j])) {
-                    arr[i, j] =arr[i, k] + arr[k, j];
+double* apsp(double* arr) {
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (arr[i][j] > (arr[i][k] + arr[k][j])) {
+                    arr[i][j] = (arr[i][k] + arr[k][j]);
                 }
             }
         }
     }
-    return arr
+    return arr;
 }
