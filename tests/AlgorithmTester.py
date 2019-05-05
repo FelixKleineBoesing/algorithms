f4b8016a3643ff46e4e9ad4c9b8aa25a4de9fd8a
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-05-05 19:22:39 +0200

    test c all pair shortest path

--- tests/AlgorithmTester.py
+++ tests/AlgorithmTester.py
@@ -1,9 +1,27 @@
-from algorithms import multiply_integers
+from algorithms._apsp import apsp
+import numpy as np
 
+def test_c_apsp():
+    dist = np.array([[0,np.Inf,3.4], [2.2,0,4.5], [6.6,np.Inf,0]])
+    print(dist)
+    apsp(dist, 3)
+    print(dist)
 
-
+    with open("../../algorithms/data/g3.txt") as f:
+        lines = f.readlines()
+    number_vertices, _ = (int(num) for num in lines.pop(0).split())
+    distance_matrix = np.full((number_vertices, number_vertices), np.inf)
+    for line in lines:
+        values = [int(num) for num in line.split()]
+        distance_matrix[values[0] - 1, values[1] - 1] = values[2]
+    for i in range(distance_matrix.shape[0]):
+        distance_matrix[i, i] = 0
+    dist = distance_matrix
+    print(dist)
+    new_dist = apsp(dist, 3)
+    print(dist)
+    print(new_dist)
 
 
 if __name__=="__main__":
-    print(multiply_integers("1","2"))
-    print(multiply_integers("1", "2"))
+    print(test_c_apsp())
